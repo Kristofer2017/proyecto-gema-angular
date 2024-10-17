53feb901d0ce1ad260093029b7c129a89cb0e018
kristofer2017 <00022920@uca.edu.sv>
2024-10-17 10:42:47 -0600

    uploading changes

--- src/app/paginas/cursos/cursos.component.ts
+++ src/app/paginas/cursos/cursos.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
+import { faCartShopping, faCircleCheck } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-cursos',
   standalone: true,
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule,],
   templateUrl: './cursos.component.html',
   styleUrl: './cursos.component.css'
 })
 export default class CursosComponent {
   iconoCarrito = faCartShopping;
+  iconoCheck = faCircleCheck;
 }
